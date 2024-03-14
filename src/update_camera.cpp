ee9e4310edb17a8528026b1ab7722a9df88b614b
nara atthama <nara.atthama-2020@ftmm.unair.ac.id>
2024-03-14 19:36:06 +0700

    fix: update_camera segfault
    segfault because of wrong data assignment

--- src/update_camera.cpp
+++ src/update_camera.cpp
@@ -1,45 +1,67 @@
 #include <ros/ros.h>
 #include <gazebo_msgs/SetModelConfiguration.h>
 #include <std_msgs/Float32.h>
+#include <boost/algorithm/string.hpp>
 
 
 std_msgs::Float32 angle;
-ros::NodeHandle *n_ptr;
 
-void camera_angle_callback(const std_msgs::Float32::ConstPtr& msg){
-    angle = *msg;
-}
 
-void update_camera_angle(){
-    ros::NodeHandle n = *n_ptr;
+void camera_angle_callback(const std_msgs::Float32::ConstPtr& msg);
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "update_camera");
+    ROS_INFO("ROS INITIALIZED");
+    ros::NodeHandle n;
+
+    ROS_INFO("Waiting 5s");
+    ros::Duration(5).sleep();
+    ROS_INFO("servo angle Subscriber created");
+    ros::Subscriber camera_sub = n.subscribe("/krbai/servo_angle", 1000, camera_angle_callback);
+    ROS_INFO("Creating SetModelConfiguration service client");
     ros::ServiceClient set_model_state = n.serviceClient<gazebo_msgs::SetModelConfiguration>("/gazebo/set_model_configuration");
+
+    ROS_INFO("model");
     gazebo_msgs::SetModelConfiguration model;
+    ROS_INFO("model created");
+
+    model.request.model_name = "robot";
+    ROS_INFO("model name set");
+    std::string joint_name = "robot::camera_joint";
+
     
-    
-    if(angle.data){
+    model.request.joint_names.push_back(joint_name);
+    ROS_INFO("Joint name set");
+
+    model.request.joint_positions.push_back(angle.data);
+    ROS_INFO("angle data set");
+    set_model_state.call(model);
+    ROS_INFO("SetModelConfiguration service called");
+    ROS_INFO("=camera update= LOOP STARTED");
+    ros::Rate loop_rate(20);
+
+    while(ros::ok()){
         if(angle.data > 1.5707){
             angle.data = 1.5707;
         }
         else if(angle.data < -1.5707){
             angle.data = -1.5707;
         }
-
-        model.request.model_name = "robot";
-        model.request.joint_names[0] = "robot::camera_joint";
-        model.request.joint_positions[0] = angle.data;
+        model.request.joint_positions.pop_back();        
+        model.request.joint_positions.push_back(angle.data);
         set_model_state.call(model);
+
+        ROS_INFO_THROTTLE(1, "update_camera loop");
+        
+        ros::spinOnce();
+        loop_rate.sleep();  
     }
+
 }
 
-int main(int argc, char **argv){
-    ros::init(argc, argv, "update_camera");
-    ros::NodeHandle n;
-    n_ptr = &n;
-    ros::topic::waitForMessage<std_msgs::Float32>("/krbai/servo_angle");
-    ros::Duration(30).sleep();
-    ros::Subscriber camera_sub = n.subscribe("/krbai/servo_angle", 10, camera_angle_callback);
 
-    update_camera_angle();
-
-    ros::spin();
+void camera_angle_callback(const std_msgs::Float32::ConstPtr& msg){
+    ROS_INFO_THROTTLE(0.3, "Camera angle: %f", msg->data);
+    angle.data = msg->data;
+ 
 }
